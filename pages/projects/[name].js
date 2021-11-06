a2c66b9eb4cac487c9b2bfde7d81dcafb3d28f24
TheBlueBurger <58081413+TheBlueBurger@users.noreply.github.com>
2021-11-06 18:48:24 +0100

    fixes

--- pages/projects/[name].js
+++ pages/projects/[name].js
@@ -1,18 +1,17 @@
-import {getProject} from "../api/projects";
+import {getProject, getProjects} from "../api/projects";
 import Head from "next/head";
 
 export default function Project({project}) {
   return <>
   <Head>
-    <title>{project.name} - My projects</title>
+    <title>{project?.name} - My projects</title>
   </Head>
     <p>{JSON.stringify(project)}</p>
   </>
 }
 
-export async function getServerSideProps({ params }) {
-  let project = getProject(params.name)
-  console.log(params.name)
+export async function getStaticProps({ params }) {
+  let project = getProject(params?.name)
   if(!project) return {
     redirect: {
       destination: "/projects",
@@ -23,5 +22,13 @@ export async function getServerSideProps({ params }) {
     props: {
       project: project
     }
+  }
+}
+
+export async function getStaticPaths() {
+  let projects = getProjects().map(project => project.id);
+  return {
+    paths: projects.map(project => ({params: {name: project}})),
+    fallback: true
   }
 }
